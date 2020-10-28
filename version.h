72e9fe1334a0d811198d4cd5e7ef9c523ec17c8b
suma <920023788@qq.com>
2020-10-28 16:28:26 +0800

    sobel块划分剪枝算法加入跳过NO_SPLIT的决策

---
+++ version.h
@@ -0,0 +1,17 @@
+// ===========================================================================
+// version.h
+// - collection of version numbers
+// ===========================================================================
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VER_MAJOR  1                // major version number
+#define VER_MINOR  2                // minor version number
+#define VER_BUILD  199             // build number
+
+#define VERSION_TYPE "release"
+#define VERSION_STR  "1.2.199"
+#define VERSION_SHA1 "457b5c9fa9e491be63104461c2c5d8908df373f5"
+
+#endif // __VERSION_H__
